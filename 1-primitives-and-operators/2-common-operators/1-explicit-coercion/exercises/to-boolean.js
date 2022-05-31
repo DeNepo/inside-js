d0c764337202761f23c44f24d0077e5eff4a6bbf
NazgulM <92852651+NazgulM@users.noreply.github.com>
2022-05-31 22:05:49 +0200

    primitives

--- 1-primitives-and-operators/2-common-operators/1-explicit-coercion/exercises/to-boolean.js
+++ 1-primitives-and-operators/2-common-operators/1-explicit-coercion/exercises/to-boolean.js
@@ -4,22 +4,31 @@
 
 console.log('-- begin --');
 
-console.assert(Boolean(undefined) === __, 'Test 1');
+console.assert(Boolean(undefined) === false, 'Test 1');
+console.log(Boolean(undefined) === false, 'Test 1');
 
-console.assert(Boolean(null) === __, 'Test 2');
+console.assert(Boolean(null) === false, 'Test 2');
+console.log(Boolean(null) === false, 'Test 2');
 
-console.assert(Boolean(-1) === __, 'Test 3');
+console.assert(Boolean(-1) === true, 'Test 3');
+console.log(Boolean(-1) === true, 'Test 3');
 
-console.assert(Boolean(0) === __, 'Test 4');
+console.assert(Boolean(0) === false, 'Test 4');
+console.log(Boolean(0) === false, 'Test 4');
 
-console.assert(Boolean(1) === __, 'Test 5');
+console.assert(Boolean(1) === true, 'Test 5');
+console.log(Boolean(1) === true, 'Test 5');
 
-console.assert(Boolean('-1') === __, 'Test 6');
+console.assert(Boolean('-1') === true, 'Test 6');
+console.log(Boolean('-1') === true, 'Test 6');
 
-console.assert(Boolean('0') === __, 'Test 7');
+console.assert(Boolean('0') === true, 'Test 7');
+console.log(Boolean('0') === true, 'Test 7');
 
-console.assert(Boolean('') === __, 'Test 8');
+console.assert(Boolean('') === false, 'Test 8');
+console.log(Boolean('') === false, 'Test 8');
 
-console.assert(Boolean(NaN) === __, 'Test 9');
+console.assert(Boolean(NaN) === false, 'Test 9');
+console.log(Boolean(NaN) === false, 'Test 9');
 
 console.log('-- end --');
